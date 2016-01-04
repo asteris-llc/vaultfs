890945b874caf5df4c3a4744089626d129dff1d2
Brian Hicks <brian@brianthicks.com>
2016-01-04 13:39:20 -0600

    cmd(root): fill in description

--- cmd/root.go
+++ cmd/root.go
@@ -27,13 +27,8 @@ var cfgFile string
 // This represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "docker-volume-vault",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "use Docker's volumes to mount Vault secrets",
+	Long:  `use Docker's volumes to mount Vault secrets`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
