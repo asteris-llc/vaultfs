67b618b32b6c72ea27fa764368967a11d73579ee
Dacode45 <ayekedavid@yahoo.com>
2016-07-06 14:48:14 -0500

    Reverted change where socket was closed before unmounting.
    This change cause `vault mount` to hang when attempting to kill the
    process.

--- fs/fs.go
+++ fs/fs.go
@@ -70,12 +70,12 @@ func (v *VaultFS) Unmount() error {
 		return errors.New("not mounted")
 	}
 
-	err := v.conn.Close()
+	err := fuse.Unmount(v.mountpoint)
 	if err != nil {
 		return err
 	}
 
-	err = fuse.Unmount(v.mountpoint)
+	err = v.conn.Close()
 	if err != nil {
 		return err
 	}
